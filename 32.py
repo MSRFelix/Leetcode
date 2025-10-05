fd452c787e6e4b52912a719fcaec17f4653dfa92
Felix Langhoff <225382920+MSRFelix@users.noreply.github.com>
2025-10-05 16:43:52 +0200

    hard python: string parsing

---
+++ 32.py
@@ -0,0 +1,30 @@
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        i = 0
+        j = 0
+        l = len(s)
+        maxi = 0
+        if l > 0 and s[0] == ")":
+            while j < l and s[j] == ")":
+                j+= 1
+        if l > 0 and s[l-1] == "(":
+            while l > 0 and s[l-1] == "(":
+                l -= 1
+        while j < l:
+            if maxi > l-j:
+                break
+            open = 0
+            for k in range(j, len(s)):
+                if s[k] == "(":
+                    open += 1
+                    continue
+                else:
+                    open -= 1
+                if open < 0:
+                    break
+                if k - j + 1 > maxi and open == 0:
+                    maxi = k - j + 1
+            j += 1
+        return maxi
+
+# finding longest valid parentheses in string
